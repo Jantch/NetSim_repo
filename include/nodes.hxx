9cc9f40e256565069ccd326910f4cc3b0f71fbf0
Hubert <hubert.wawak88@gmail.com>
2026-01-15 13:41:58 +0100

    Added googletest-master, configure CMake, nodes header skeleton

---
+++ include/nodes.hxx
@@ -0,0 +1,51 @@
+#pragma once
+#include <map>
+#include <memory>
+#include <optional>
+#include "package.hxx"
+#include "storage_types.hxx"
+#include "helpers.hxx"
+#include "types.hxx"
+
+enum class ReceiverType {
+    WORKER,
+    STOREHOUSE
+};
+
+class IPackageReceiver {
+public:
+    using const_iterator = IPackageStockpile::const_iterator;
+
+    virtual void receive_package(Package&& p) = 0;
+    virtual ElementID get_id() const = 0;
+    virtual ReceiverType get_receiver_type() const = 0;
+
+    virtual const_iterator cbegin() const = 0;
+    virtual const_iterator cend() const = 0;
+    virtual const_iterator begin() const = 0;
+    virtual const_iterator end() const = 0;
+
+    virtual ~IPackageReceiver() = default;
+};
+
+class ReceiverPreferences {
+public:
+    using preferences_t = std::map<IPackageReceiver*, double>;
+    using const_iterator = preferences_t::const_iterator;
+
+    ReceiverPreferences(ProbabilityGenerator pg = probability_generator);
+
+    void add_receiver(IPackageReceiver* r);
+    void remove_receiver(IPackageReceiver* r);
+    IPackageReceiver* choose_receiver();
+    const preferences_t& get_preferences() const;
+
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+    const_iterator begin() const;
+    const_iterator end() const;
+
+private:
+    ProbabilityGenerator pg_;
+    preferences_t preferences_;
+};
